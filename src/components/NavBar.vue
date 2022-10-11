cff003e504f100455cbf3ff85375f0b5aab8d234
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-11 17:29:13 +0200

    navbar content and style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,73 @@
 <script setup>
 defineProps({
-  msg: {
-    type: String,
+  points: {
+    type: Number,
     required: true
   }
 })
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <main>
+    <div id="menu">
+      <a href="/">Open Source Casino</a>
+      <a href="/about">About</a>
+    </div>
+
+    <div id="pointCounter">
+      <p id="pointCounterContent">Points: {{ points }}</p>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+* {
+  width: 100%;
+  background-color: #121212;
+  color: azure;
+  padding: 0%;
+  overflow: hidden;
+}
 
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+a {
+  text-decoration: none;
+  color: aquamarine;
+  margin-left: 10%;
+}
+
+div {
+  padding: 1%;
+  height: 100%;
+}
+
+#menu {
+  width: 48%;
+  display: inline-block;
+  font-size: 150%;
+}
+
+#pointCounter {
+  display: inline-block;
+  width: 48%;
+  text-align: right;
+  font-size: 150%;
+}
+
+@media screen and (min-width: 901px) {
+  #menu {
+    font-size: 150%;
+  }
+}
+
+@media screen and (max-width: 900px) {
+  #menu {
+    font-size: 80%;
+  }
+}
 </style>
