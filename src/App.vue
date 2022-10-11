d4e4edbdf0d5ec3662985425651ce82c5f12e3c5
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-11 16:20:05 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
+
+</script>
+
+<template>
+  <header>
+    <NavBar msg="You did it!" />
+  </header>
+
+  <RouterView />
+
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>
