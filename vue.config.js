e454e4cbfe62bf0aa849ea3634a1db076240f1d9
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-11 22:50:29 +0200

    changed vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "OpenSourceCasino" : "/",
-};
+    "publicPath": '/opensourcecasino/'
+}
