44c43417c9d3bfb7dad2179668753d7ad2c94db2
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-11 22:10:18 +0200

    installed execa, deployment script

---
+++ scripts/deploy-gp.js
@@ -0,0 +1,29 @@
+import {
+    execa
+} from 'execa';
+
+const buildFolderName = "dist";
+const tmpBranchName = "gh-pages";
+
+(async () => {
+    try {
+        await execa("git", ["checkout", "--orphan", tmpBranchName]);
+
+        console.log("Build");
+        await execa("npm", ["run", "build"]);
+        await execa("git", ["--work-tree", buildFolderName, "add", "--all"]);
+        await execa("git", ["--work-tree", buildFolderName, "commit", "-m", "gp-deployment"]);
+
+        console.log("Github Pages deployment");
+        await execa("git", ["push", "origin", "HEAD:" + tmpBranchName, "--force"]);
+
+        console.log("Clean up");
+        await execa("git", ["checkout", "-f", "main"]);
+        await execa("git", ["branch", "-d", tmpBranchName]);
+
+        console.log("Finished");
+    } catch (err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+})();
