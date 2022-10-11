680c15165524d16aa2871115eb5cf87bad199889
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-11 21:23:48 +0200

    changed slot style

--- src/components/Slots/Slot.vue
+++ src/components/Slots/Slot.vue
@@ -9,12 +9,12 @@ export default {
         spin(spinCount) {
             const slotValueElement = this.$refs.slotValue;
             const slotValues = this.slotValues;
-            slotValueElement.style.border = "3px solid aquamarine";
+            slotValueElement.style.background = "#d7ffc0";
             
             return new Promise((resolve, reject) => {
                 function spinRec() {
                     if (spinCount <= 0) {
-                        slotValueElement.style.border = "3px solid #000000";
+                        slotValueElement.style.background = "#b4e995";
                         return resolve(slotValueElement.innerText);
                     }
                     slotValueElement.innerText = slotValues[Math.floor(Math.random() * slotValues.length)];
@@ -34,9 +34,13 @@ export default {
 <style scoped>
 #slotValue {
     color: azure;
-    background-color: #121212;
+    background-color: #b4e995;
     border: 3px solid #000000;
+    font-size: 110%;
+    border-radius: 0.7rem;
     width: 1%;
     height: 1%;
+    font-weight: 900;
+    color: black;
 }
 </style>
