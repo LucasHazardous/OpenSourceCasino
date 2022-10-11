ba837256dfb35e7a84e0278673766b1cd5d28811
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-11 19:07:29 +0200

    value changing slot element

---
+++ src/components/Slot.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    data() {
+        return {
+            slotValues: ["$", "0", "7", "-", "="]
+        }
+    },
+    methods: {
+        spin(spinCount) {
+            const slotValueElement = this.$refs.slotValue;
+            const slotValues = this.slotValues;
+            slotValueElement.style.border = "3px solid aquamarine";
+            
+            return new Promise((resolve, reject) => {
+                function spinRec() {
+                    if (spinCount <= 0) {
+                        slotValueElement.style.border = "3px solid #000000";
+                        return resolve();
+                    }
+                    slotValueElement.innerText = slotValues[Math.floor(Math.random() * slotValues.length)];
+                    setTimeout(spinRec, 200, --spinCount);
+                }
+                spinRec();
+            });
+        }
+    }
+}
+</script>
+
+<template>
+    <div ref="slotValue" id="slotValue">$</div>
+</template>
+
+<style scoped>
+#slotValue {
+    color: azure;
+    background-color: #121212;
+    border: 3px solid #000000;
+    width: 1%;
+    height: 1%;
+}
+</style>
