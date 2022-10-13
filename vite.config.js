5fce3dd4d31a5aacf16caa70f8f05b58aeeeae75
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-13 18:13:33 +0200

    changed to build with vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL
+} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -8,7 +13,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',
+        import.meta.url))
     }
   }
 })
