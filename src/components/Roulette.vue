3fefbd5305e175a924c4b66f59d136e54c19bc6d
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-14 15:50:07 +0200

    cleanup board drawing code

--- src/components/Roulette.vue
+++ src/components/Roulette.vue
@@ -6,7 +6,8 @@ export default {
             canvas: undefined,
             spinning: false,
             canvasRadius: 0.5,
-            offset: 0.05
+            offset: 0.05,
+            fieldCount: 20
         }
     },
     mounted() {
@@ -31,31 +32,30 @@ export default {
         drawFreshWheel() {
             const size = this.canvas.width;
             let r = this.canvasRadius*size;
-            
+            const centerX = size/2;
+            const centerY = size/2;
+
             this.canvasCtx.fillStyle = "black";
             this.canvasCtx.beginPath();
-            this.canvasCtx.arc(size/2, size/2, r, 0, 2 * Math.PI, false);
+            this.canvasCtx.arc(centerX, centerY, r, 0, 2 * Math.PI, false);
             this.canvasCtx.fill();
 
             this.canvasCtx.strokeStyle = "red";
             this.canvasCtx.beginPath();
-            this.canvasCtx.arc(size/2, size/2, r*(1-this.offset*2), 0, 2 * Math.PI, false);
+            this.canvasCtx.arc(centerX, centerY, r*(1-this.offset*2), 0, 2 * Math.PI, false);
             this.canvasCtx.stroke();
 
             r -= size*this.offset;
 
-            this.canvasCtx.strokeStyle = "red";
             this.canvasCtx.fillStyle = "red";
-            for(let i = 0; i < 20; i++) {
-                const pointX = r*Math.sin(2*Math.PI*i/20) + size/2;
-                const pointY = r*Math.cos(2*Math.PI*i/20) + size/2;
+            for(let i = 0; i < this.fieldCount; i++) {
+                const pointX = r*Math.sin(2*Math.PI*i/this.fieldCount) + centerX;
+                const pointY = r*Math.cos(2*Math.PI*i/this.fieldCount) + centerY;
 
                 this.canvasCtx.beginPath();
                 this.canvasCtx.moveTo(pointX, pointY);
-                this.canvasCtx.lineTo(size/2, size/2);
+                this.canvasCtx.lineTo(centerX, centerY);
                 this.canvasCtx.stroke();
-
-                console.log(this.canvasCtx.getImageData(pointX, pointY, 1, 1))
             }
         }
     }
