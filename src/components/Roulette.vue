4d503ca25a5450456b33eb71a9431cd327d7341c
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-14 15:43:58 +0200

    draw basic wheel outline

--- src/components/Roulette.vue
+++ src/components/Roulette.vue
@@ -3,7 +3,10 @@ export default {
     data() {
         return {
             canvasCtx: undefined,
-            canvas: undefined
+            canvas: undefined,
+            spinning: false,
+            canvasRadius: 0.5,
+            offset: 0.05
         }
     },
     mounted() {
@@ -12,14 +15,48 @@ export default {
 
         window.addEventListener("resize", this.rescaleCanvas);
         this.rescaleCanvas();
+        this.drawFreshWheel();
     },
     methods: {
         spin() {
-            
+            this.drawFreshWheel();
+            this.spinning = true;
         },
         rescaleCanvas() {
+            if(this.spinning) return;
             this.canvas.width = window.innerHeight/3.5;
             this.canvas.height = window.innerHeight/3.5;
+            this.drawFreshWheel();
+        },
+        drawFreshWheel() {
+            const size = this.canvas.width;
+            let r = this.canvasRadius*size;
+            
+            this.canvasCtx.fillStyle = "black";
+            this.canvasCtx.beginPath();
+            this.canvasCtx.arc(size/2, size/2, r, 0, 2 * Math.PI, false);
+            this.canvasCtx.fill();
+
+            this.canvasCtx.strokeStyle = "red";
+            this.canvasCtx.beginPath();
+            this.canvasCtx.arc(size/2, size/2, r*(1-this.offset*2), 0, 2 * Math.PI, false);
+            this.canvasCtx.stroke();
+
+            r -= size*this.offset;
+
+            this.canvasCtx.strokeStyle = "red";
+            this.canvasCtx.fillStyle = "red";
+            for(let i = 0; i < 20; i++) {
+                const pointX = r*Math.sin(2*Math.PI*i/20) + size/2;
+                const pointY = r*Math.cos(2*Math.PI*i/20) + size/2;
+
+                this.canvasCtx.beginPath();
+                this.canvasCtx.moveTo(pointX, pointY);
+                this.canvasCtx.lineTo(size/2, size/2);
+                this.canvasCtx.stroke();
+
+                console.log(this.canvasCtx.getImageData(pointX, pointY, 1, 1))
+            }
         }
     }
 }
@@ -31,6 +68,7 @@ export default {
 
 <style scoped>
 canvas {
+    margin-top: 5%;
     background-color: azure;
 }
 </style>
