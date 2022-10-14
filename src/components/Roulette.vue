daf6c65363663bbe405cdff4d9522355d6429644
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-14 15:04:32 +0200

    roulette element with autoscaling canvas

---
+++ src/components/Roulette.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    data() {
+        return {
+            canvasCtx: undefined,
+            canvas: undefined
+        }
+    },
+    mounted() {
+        this.canvas = this.$refs.roulette;
+        this.canvasCtx = this.canvas.getContext("2d");
+
+        window.addEventListener("resize", this.rescaleCanvas);
+        this.rescaleCanvas();
+    },
+    methods: {
+        spin() {
+            
+        },
+        rescaleCanvas() {
+            this.canvas.width = window.innerHeight/3.5;
+            this.canvas.height = window.innerHeight/3.5;
+        }
+    }
+}
+</script>
+
+<template>
+    <canvas ref="roulette"></canvas>
+</template>
+
+<style scoped>
+canvas {
+    background-color: azure;
+}
+</style>
