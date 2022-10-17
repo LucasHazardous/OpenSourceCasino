63f49c1841e775d31b3f0f5d42ec26916e3213c4
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-17 16:33:59 +0200

    blackjack in available games

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
   data() {
     return {
       points: Number(localStorage.getItem("points")),
-      games: ["Slots", "Roulette"]
+      games: ["Slots", "Roulette", "Blackjack"]
     }
   },
   methods: {
