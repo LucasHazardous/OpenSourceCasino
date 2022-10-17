4b28c502620dcc97c7bbf662ca58d804ce23aa3c
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-17 16:33:07 +0200

    element for displaying cards

---
+++ src/components/BlackjackCardTable.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    props: ["cards"]
+}
+</script>
+
+<template>
+    <div id="cardContainer">
+        {{ cards.join(" ") }}
+    </div>
+</template>
+
+<style scoped>
+</style>
