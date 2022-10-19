23f5107e98a91c66e1d9dd5a7f98ee161ca637a2
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-19 14:04:45 +0200

    card symbols, new cardTable look

--- src/components/BlackjackCardTable.vue
+++ src/components/BlackjackCardTable.vue
@@ -5,13 +5,51 @@ export default {
 </script>
 
 <template>
-    <div id="cardContainer">
-        {{ dealerHide ? cards[0] + " | ?" : cards.join(" | ") }}
+    <div class="cardTable">
+        <div v-for="card in (dealerHide ? cards.slice(0, 1) : cards)" class="cardContainer">
+            <div class="upValue">{{ card[2] }}</div>
+            <div class="img"><img :src="'/symbols/' + card[0] + '.png'"></div>
+            <div class="downValue">{{ card[2] }}</div>
+        </div>
+        <div v-if="dealerHide" class="cardContainer">
+            <div class="upValue">?</div>
+            <div class="imgUnknown">?</div>
+            <div class="downValue">?</div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-#cardContainer {
-    color: azure;
+.cardTable {
+    padding: 1%;
+}
+
+.cardContainer {
+    background-color: azure;
+    text-align: center;
+    display: inline-block;
+    outline: 2px solid black;
+    max-width: 6rem;
+    width: 15%;
+    margin-left: 1%;
+}
+
+.upValue {
+    text-align: left;
+}
+
+.img {
+    text-align: center;
+    margin-top: 2rem;
+}
+
+.imgUnknown {
+    height: 37.5px;
+    margin-top: 2rem;
+}
+
+.downValue {
+    text-align: right;
+    margin-top: 2rem;
 }
 </style>
