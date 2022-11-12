16402b406dfa3f39b1344307b6a2eeb96ccf4321
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-11-12 21:25:03 +0100

    changed loot table icons
    Co-Authored-By: Matthew Pasek <92646515+matt-pasek@users.noreply.github.com>

--- src/views/RouletteView.vue
+++ src/views/RouletteView.vue
@@ -127,7 +127,7 @@ export default {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-current"
+                  class="inline-block w-8 h-8 stroke-current stroke-primary"
                 >
                   <path
                     stroke-linecap="round"
@@ -147,13 +147,13 @@ export default {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-current"
+                  class="inline-block w-8 h-8 stroke-current stroke-primary"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
               </div>
--- src/views/SlotsView.vue
+++ src/views/SlotsView.vue
@@ -150,7 +150,7 @@ export default {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-current"
+                  class="inline-block w-8 h-8 stroke-current stroke-primary"
                 >
                   <path
                     stroke-linecap="round"
@@ -177,13 +177,13 @@ export default {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-current"
+                  class="inline-block w-8 h-8 stroke-current stroke-primary"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
               </div>
@@ -204,13 +204,13 @@ export default {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-current"
+                  class="inline-block w-8 h-8 stroke-current stroke-primary"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
               </div>
