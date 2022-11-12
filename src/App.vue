2f0fe62a3d3c9fb85b318f003556845bb03fccdb
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-11-12 21:22:29 +0100

    changed home images
    Co-Authored-By: Matthew Pasek <92646515+matt-pasek@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import Game from "@/Game";
 // make a list of games
 const games = [
   new Game("Slots", "/src/assets/slots.jpg", "Spin the wheel and win!"),
-  new Game("Roulette", "/src/assets/slots.jpg", "Spin the wheel and win!"),
-  new Game("Blackjack", "/src/assets/slots.jpg", "Beat the dealer and win!"),
+  new Game("Roulette", "/src/assets/roulette.jpg", "Spin the wheel and win!"),
+  new Game("Blackjack", "/src/assets/blackjack.jpg", "Beat the dealer and win!"),
 ];
 export default {
   data() {
