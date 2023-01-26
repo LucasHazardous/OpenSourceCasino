e109529afa71abf7ce208973c4b6d4a73202933b
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-01-26 18:44:15 +0100

    moved info section into a separate component

---
+++ src/components/InfoComponent.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: ["elements"],
+};
+</script>
+
+<template>
+  <div tabindex="0" class="collapse rounded-box">
+    <div class="collapse-title text-2xl font-medium text-center p-5">
+      Loot chances
+    </div>
+    <div class="collapse-content">
+      <div class="stats stats-vertical lg:stats-horizontal shadow">
+        <div class="stat" v-for="element in elements">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-8 h-8 stroke-primary"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </div>
+          <div class="stat-title">{{ element.title }}</div>
+          <div class="stat-value">{{ element.value }}</div>
+          <div class="stat-desc">{{ element.desc }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/SlotsView.vue
+++ src/views/SlotsView.vue
@@ -1,6 +1,7 @@
 <script>
 import SlotComponent from "../components/SlotComponent.vue";
 import BetSelectButton from "../components/BetSelectButton.vue";
+import InfoComponent from "../components/InfoComponent.vue";
 import ToastNotification from "@/components/ToastNotification.vue";
 
 export default {
@@ -23,6 +24,7 @@ export default {
     ToastNotification,
     SlotComponent,
     BetSelectButton,
+    InfoComponent,
   },
   methods: {
     spinSlots(slotCounter, rewards = []) {
@@ -135,103 +137,43 @@ export default {
           </button>
         </div>
       </div>
-      <div tabindex="0" class="collapse rounded-box">
-        <div class="collapse-title text-2xl font-medium text-center p-5">
-          Loot chances
-        </div>
-        <div class="collapse-content">
-          <div class="stats stats-vertical lg:stats-horizontal shadow">
-            <div class="stat">
-              <div class="stat-figure text-secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-primary"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  ></path>
-                </svg>
-              </div>
-              <div class="stat-title">$$$</div>
-              <div class="stat-value">
-                {{
-                  (
-                    (1 / Math.pow(this.slotValues.length, this.slotAmount)) *
-                    100
-                  ).toFixed(2)
-                }}%
-              </div>
-              <div class="stat-desc">Reward: Original bet x 5</div>
-            </div>
-            <div class="stat">
-              <div class="stat-figure text-secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-primary"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  ></path>
-                </svg>
-              </div>
-              <div class="stat-title">777</div>
-              <div class="stat-value">
-                {{
-                  (
-                    (1 / Math.pow(this.slotValues.length, this.slotAmount)) *
-                    100
-                  ).toFixed(2)
-                }}%
-              </div>
-              <div class="stat-desc">Reward: Original bet x 7</div>
-            </div>
-            <div class="stat">
-              <div class="stat-figure text-secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  class="inline-block w-8 h-8 stroke-primary"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  ></path>
-                </svg>
-              </div>
-              <div class="stat-title">==</div>
-              <div class="stat-value">
-                {{
-                  (
-                    (1 -
-                      (Math.pow(this.slotValues.length - 1, this.slotAmount) +
-                        Math.pow(
-                          this.slotValues.length - 1,
-                          this.slotAmount - 1
-                        ) *
-                          this.slotAmount) /
-                        Math.pow(this.slotValues.length, this.slotAmount)) *
-                    100
-                  ).toFixed(2)
-                }}%
-              </div>
-              <div class="stat-desc">Reward: Original bet x 2</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <InfoComponent
+        :elements="[
+          {
+            title: '$$$',
+            value:
+              (
+                (1 / Math.pow(this.slotValues.length, this.slotAmount)) *
+                100
+              ).toFixed(2) + '%',
+            desc: 'Reward: Original bet x 5',
+          },
+
+          {
+            title: '777',
+            value:
+              (
+                (1 / Math.pow(this.slotValues.length, this.slotAmount)) *
+                100
+              ).toFixed(2) + '%',
+            desc: 'Reward: Original bet x 7',
+          },
+
+          {
+            title: '==',
+            value:
+              (
+                (1 -
+                  (Math.pow(this.slotValues.length - 1, this.slotAmount) +
+                    Math.pow(this.slotValues.length - 1, this.slotAmount - 1) *
+                      this.slotAmount) /
+                    Math.pow(this.slotValues.length, this.slotAmount)) *
+                100
+              ).toFixed(2) + '%',
+            desc: 'Reward: Original bet x 2',
+          },
+        ]"
+      ></InfoComponent>
     </div>
   </main>
 </template>
