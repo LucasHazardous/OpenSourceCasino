f9e14a52f1ebccffce8b7e8b47b2c52884e356b2
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-03-04 13:38:20 +0100

    vite applied pwa plugin and changed out dir structure

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,33 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   base: "/OpenSourceCasino/",
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "img/[name]-[hash][extname]";
+          } else if (/\.css$/.test(name ?? "")) {
+            return "css/[name]-[hash][extname]";
+          } else if (/\.js$/.test(name ?? "")) {
+            return "js/[name]-[hash][extname]";
+          }
+          return "[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
