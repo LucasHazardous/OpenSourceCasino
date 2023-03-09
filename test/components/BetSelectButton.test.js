7f97d0480fde73e347439328c6ac9bc170e4c954
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-03-09 22:19:09 +0100

    change select value

--- test/components/BetSelectButton.test.js
+++ test/components/BetSelectButton.test.js
@@ -1,8 +1,7 @@
 import { mount } from "@vue/test-utils";
-import { expect } from "chai";
 import BetSelectButton from "../../src/components/BetSelectButton.vue";
 
-test("mount component", async () => {
+test("mount component and change value", async () => {
   const bets = [100, 2000, 5000];
 
   expect(BetSelectButton).toBeTruthy();
@@ -20,4 +19,8 @@ test("mount component", async () => {
   for (let i = 0; i < options.length; i++) {
     expect(Number(options[i].text())).toBe(bets[i]);
   }
+
+  await wrapper.get("select").setValue(bets[1]);
+
+  expect(Number(wrapper.getCurrentComponent().data.value)).toBe(bets[1]);
 });
